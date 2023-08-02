461532aafb4847694cee99c864ff0267f3b094a0
NigarValikhanova <140783772+NigarValikhanova@users.noreply.github.com>
2023-08-02 15:12:42 +0400

    Lesson 1

---
+++ Lesson 1/Test 1.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Test
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Ad daxil et");
+			string word = Console.ReadLine();
+			Console.WriteLine("Hello " + word);
+			Console.ReadLine();
+		}
+	}
+}
+
