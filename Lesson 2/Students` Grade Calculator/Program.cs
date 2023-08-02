6f397db4da360a4e2d94bc35f608464de700a163
NigarValikhanova <140783772+NigarValikhanova@users.noreply.github.com>
2023-08-02 15:13:44 +0400

    Lesson 2

---
+++ Lesson 2/Students` Grade Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.Design;
+
+namespace Student_s_grade_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("Enter your score, please");
+            int score = Convert.ToInt32(Console.ReadLine());
+            if (score >= 90)
+            {
+                Console.WriteLine("Your grade is A");
+            }
+            else if (score >= 80)
+            {
+                Console.WriteLine("Your grade is B");
+            }
+            else if (score >= 70)
+            {
+                Console.WriteLine("Your score is C");
+            }
+            else if (score >= 60)
+            {
+                Console.WriteLine("Your score is D");
+            }
+            else if (score >= 0)
+            {
+                Console.WriteLine("Your score is E");
+            }
+
+            Console.WriteLine("Hope your score satisfies you");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Lesson 2/Ticket Price Calculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace Ticket_Price_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the 'Watch Anything You Want With US' movie theater.");
+            Console.WriteLine("If you want a ticket first you should decide which day you want to watch. Because we have a great discount on every Wednesday.");
+            Console.WriteLine("So first, please, enter the weekday you want to watch");
+            string weekday = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("All movies tickets are the same price. But our tickets may vary in price only in accordance with your age. So please enter your age");
+            var age = Convert.ToInt32(Console.ReadLine());
+            if (weekday == "Wednesday")
+            {
+                if (age >= 65)
+                {
+                    Console.WriteLine("We have $2 discount for you. Your ticket price is $5");
+                }
+                else if (age >= 13)
+                {
+                    Console.WriteLine("We have $2 discount for you. Your ticket price is $8");
+                }
+                else if (age >= 0)
+                {
+                    Console.WriteLine("We have $2 discount for you. Your ticket price is $3");
+                }
+
+            }
+            else if (weekday != "Wednesday")
+            {
+                if (age >= 65)
+                {
+                    Console.WriteLine("Your ticket price is $7");
+                }         
+                else if (age >= 13)
+                {
+                    Console.WriteLine("Your ticket price is $10");
+                }
+                else if (age >= 0)
+                {
+                    Console.WriteLine("Your ticket price is $5");
+                }
+            }
+            Console.WriteLine.("Enjoy your movie");
+            Console.ReadLine();
+        }
+    }
+}
