1b18ad8a6799b7d0b9b8617b4292b3b5d21b92e0
NigarValikhanova <140783772+NigarValikhanova@users.noreply.github.com>
2023-08-29 06:31:12 +0400

    Quiz Task

---
+++ Lesson 9/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace QuizTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NewMethods.checkingmethod());
+        }
+
+    }
+    static class NewMethods
+    { 
+        //my quiz task
+        public static bool checkingmethod()
+        {
+            Console.WriteLine("Enter the something you want to check");
+            string something = Console.ReadLine();
+
+            bool Digit = false;
+            bool Letter = false;
+            foreach (char item in something)
+            {
+                if ((int)item >= 65 && (int)item <= 90 || (int)item >= 97 && (int)item <= 122)
+                {
+                    Letter = true;
+                }
+                if ((int)item >= 48 && (int)item <= 57)
+                {
+                    Digit = true;
+                }
+
+            }
+            if (Digit == true && Letter == true)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
